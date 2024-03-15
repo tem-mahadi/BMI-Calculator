03aa398fdd20289c9a7f7f4110fd6951f6b4091f
Hasan Al Mahadi <mahadi4uruetcse21@gmail.com>
2024-03-16 04:27:43 +0600

    fixed

--- app/src/main/java/com/temmahadi/bmicalculator/startPage.java
+++ app/src/main/java/com/temmahadi/bmicalculator/startPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -34,15 +35,15 @@ public class startPage extends AppCompatActivity {
 
         Intent next = new Intent(this, MainActivity.class);
         spinner= findViewById(R.id.spinner);
-        autocm=findViewById(R.id.autoComplete);
+        autocm=findViewById(R.id.autocm);
         t=findViewById(R.id.text2);
 
         arr.add("?_?");
         arr.add("I Am Fine");
         arr.add("I Am Sad");
-        autoArr.add("fat. I think i got so much weight");
-        autoArr.add("skinny. I think i need to gain weight");
-        autoArr.add("normal. I think i'm cool");
+        autoArr.add("fat. I think i got so much weight!");
+        autoArr.add("skinny. I think i need to gain weight!");
+        autoArr.add("normal. I think i'm cool!");
 
         //adapter takes the elements from an array and put it to a view
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, arr);
@@ -55,10 +56,10 @@ public class startPage extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 if (i == 2) {
                     t.setText("Feeling fat or skinny, dear?");
-                    Toast.makeText(startPage.this, "WHY Are You Sad?? Say I'm Happy!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(startPage.this, "WHY Are You Sad?? Say I'm Happy!", Toast.LENGTH_LONG).show();
                 } else if (i == 1) {
-                    t.setText("Guess what you're (fat/skinny/normal)");
-                    Toast.makeText(startPage.this, "It's Good To see You Happy!", Toast.LENGTH_SHORT).show();
+                    t.setText("Guess what you're(fat/skinny/normal)");
+                    Toast.makeText(startPage.this, "It's Good To see You Happy!", Toast.LENGTH_LONG).show();
                 }
             }
 
@@ -72,8 +73,15 @@ public class startPage extends AppCompatActivity {
         autocm.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(startPage.this,"Let's Calculate BMI", Toast.LENGTH_SHORT).show();
-                startActivity(next); finish();
+                Toast.makeText(startPage.this,"Let's Calculate BMI", Toast.LENGTH_LONG).show();
+                new Handler().postDelayed(new Runnable() { //delay 3s before running run() method
+                    @Override
+                    public void run() {
+                        startActivity(next);
+                        finish();
+                    }
+                },1000);
+
             }
         });
     }
