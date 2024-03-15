6a6a8cd191906092e2cac432317d78d17316f791
Hasan Al Mahadi <mahadi4uruetcse21@gmail.com>
2024-03-16 03:49:45 +0600

    fixed

--- app/src/main/java/com/temmahadi/bmicalculator/startPage.java
+++ app/src/main/java/com/temmahadi/bmicalculator/startPage.java
@@ -21,8 +21,8 @@ import com.airbnb.lottie.LottieAnimationView;
 import java.util.ArrayList;
 
 public class startPage extends AppCompatActivity {
-    Spinner spinner; AutoCompleteTextView autocm;
-    LottieAnimationView lottie;
+    Spinner spinner;
+    AutoCompleteTextView autocm;
     TextView t;
     ArrayList<String> arr = new ArrayList<>();
     ArrayList<String> autoArr = new ArrayList<>();
@@ -37,12 +37,12 @@ public class startPage extends AppCompatActivity {
         autocm=findViewById(R.id.autoComplete);
         t=findViewById(R.id.text2);
 
+        arr.add("?_?");
         arr.add("I Am Fine");
         arr.add("I Am Sad");
         autoArr.add("fat. I think i got so much weight");
         autoArr.add("skinny. I think i need to gain weight");
         autoArr.add("normal. I think i'm cool");
-
 
         //adapter takes the elements from an array and put it to a view
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, arr);
@@ -50,26 +50,30 @@ public class startPage extends AppCompatActivity {
         spinner.setAdapter(adapter);
         autocm.setAdapter(adapter2); autocm.setThreshold(3);
 
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                if(position==0){
-                    lottie.playAnimation();
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 2) {
                     t.setText("Feeling fat or skinny, dear?");
-                    Toast.makeText(startPage.this,"WHY Are You Sad?? Say I'm Happy!", Toast.LENGTH_SHORT).show();
-                }
-                if(position==1){
-                    t.setText("Guess what you're(fat/skinny/normal)");
-                    Toast.makeText(startPage.this,"It's Good To see You Happy!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(startPage.this, "WHY Are You Sad?? Say I'm Happy!", Toast.LENGTH_SHORT).show();
+                } else if (i == 1) {
+                    t.setText("Guess what you're (fat/skinny/normal)");
+                    Toast.makeText(startPage.this, "It's Good To see You Happy!", Toast.LENGTH_SHORT).show();
                 }
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // This method is required but can be left empty for your case
+            }
         });
+
 
         autocm.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast.makeText(startPage.this,"Let's Calculate BMI", Toast.LENGTH_SHORT).show();
-                startActivity(next);
+                startActivity(next); finish();
             }
         });
     }
